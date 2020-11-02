70494ef3a552f227fc00f1c804a990c5ea1ede3e
TalesVGS <tales.santana@aluno.unievangelica.edu.br>
2020-11-02 18:53:18 -0300

    Alterações no front e iniciando back-end

---
+++ capacitação-biblioteca/back-end/src/main/java/br/com/capacitacaobiblioteca/CapacitacaoBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package br.com.capacitacaobiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapacitacaoBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapacitacaoBibliotecaApplication.class, args);
+	}
+
+}
---
+++ capacitação-biblioteca/back-end/src/test/java/br/com/capacitacaobiblioteca/CapacitacaoBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.capacitacaobiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapacitacaoBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
