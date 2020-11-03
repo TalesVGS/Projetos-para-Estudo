4d6e5695fdcdda8131b01209f064e3ea348cc0a7
TalesVGS <tales.santana@aluno.unievangelica.edu.br>
2020-11-03 14:46:18 -0300

    implementando service

--- capacitação-biblioteca/back-end/src/main/java/br/com/capacitacaobiblioteca/domain/Livro/LivroService.java
+++ capacitação-biblioteca/back-end/src/main/java/br/com/capacitacaobiblioteca/domain/Livro/LivroService.java
@@ -1,4 +1,18 @@
 package br.com.capacitacaobiblioteca.domain.Livro;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class LivroService {
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+    public List<Livro> findAll() {
+        return livroRepository.findAll();
+    }
+
 }
